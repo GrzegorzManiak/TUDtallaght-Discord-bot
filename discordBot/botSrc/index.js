681fd3813854332ee0168558f566eb65b7ce5da9
KetamineKyle <greg@ap3x.xyz>
2021-09-30 00:55:41 +0100

    Improved the help command, added a command to spawn the main email verification msg

--- discordBot/botSrc/index.js
+++ discordBot/botSrc/index.js
@@ -1,14 +1,17 @@
 const discordJS = require('discord.js');
-
-global.prefix = '.';
-global.commands = {};
-
 const client = new discordJS.Client({
     intents: [
         discordJS.Intents.FLAGS.GUILDS, //Give the bot access to its connected guilds, should only be one tho.
-        discordJS.Intents.FLAGS.GUILD_MESSAGES //Give it the ability to see incoming messages.
-    ]
+        discordJS.Intents.FLAGS.GUILD_MESSAGES, //Give it the ability to see incoming messages.
+        discordJS.Intents.FLAGS.GUILD_MESSAGE_REACTIONS //Gives the bot the ability to check for and add reactions
+    ],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
+
+
+global.prefix = '.';
+global.commands = {};
+global.client = client;
 
 //Authenticate the bot
 client.login(global.discord);
@@ -18,12 +21,27 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}, ${client.user.id}!`);
 });
 
+function getUserRolesFromMessage(message, roles = []) {
+    message.member.roles.cache.map(m => roles = [...roles, m.name]);
+    return roles;
+}
+
+//checks if the user has sufficient privileges to preform an action.
+function checkPermisions(roles, command, pass = false) {
+    if (command !== undefined && command.roles !== undefined && command.roles.length > 0) {
+        roles.forEach(hasRole => {
+            if (command.roles.includes(hasRole) === true) pass = true;
+        });
+    } else pass = true;
+    return pass;
+}
+
 //triggers everytime a message is sent.
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async(message) => {
     commandHandler(message);
 });
 
-function commandHandler(message, hasPremissions = false, roles = []) {
+function commandHandler(message) {
     let charArray = message.content.split('');
     if (charArray[0] !== global.prefix) return;
 
@@ -31,19 +49,47 @@ function commandHandler(message, hasPremissions = false, roles = []) {
     let splitMessage = message.content.split(' ');
 
     //gives us the acutal command name by getting the first word and dropping the prefix character
-    let command = global.commands[splitMessage[0].substring(1).toLowerCase()];
-
-    //checks if the command requires a roll to execute.
-    message.member.roles.cache.map(m => roles = [...roles, m.name]);
-
-    if (command.roles !== undefined && command.roles.length > 0) {
-        roles.forEach(hasRole => {
-            if (command.roles.includes(hasRole)) hasPremissions = true;
-        });
-    } else hasPremissions = true;
+    let command = global.commands[splitMessage[0].substring(1).toLowerCase()],
+        userRoles = getUserRolesFromMessage(message),
+        hasPremissions = checkPermisions(userRoles, command);
 
     if (hasPremissions === false) message.channel.send(`${message.member} You dont have the sufficient privileges to execute this command.`);
-    else if (command !== undefined) command.callbackFunction(splitMessage, message, roles)
+    else if (command !== undefined) command.callbackFunction(splitMessage, message, userRoles)
+}
+
+//triggers everytime a reaction is added to a msg sent from the bot
+client.on('messageReactionAdd', async(reaction, user) => {
+    reactionHandler(reaction, user, false);
+});
+
+async function reactionHandler(reaction, user, removeReaction) {
+    //[TODO]// need to make it so it checks if the user has sufficient privileges to add/remove a reaction
+    /*
+    const message = !reaction.message.author ?
+        await reaction.message.fetch() :
+        reaction.message;
+    
+    //let guild = message.gi
+
+    // if its a reaction to anything else than the bot, ignore it.
+    if (message.author.id !== client.user.id) return;
+
+    // if its the bot reacting to its self, ignore it.
+    if (message.author.id === user.id) return;
+
+    // regex that grabs the command identifier in the embeds footer.
+    const regex = /\[(.+)\]/gm;
+
+    if (message.embeds !== undefined) message.embeds.forEach(embed => {
+        let commandRefrence = regex.exec(embed.footer.text)[1].split(',');
+
+        if (Object.keys(global.commands).includes(commandRefrence[0])) {
+            //console.log(reaction.users.reaction.users.reaction.message)
+                //user.member.roles.cache.map(m => console.log(m.name))
+                //let hasPremissions = checkPermisions(reaction.message.author, global.commands[commandRefrence[0]]);
+                //console.log(hasPremissions, getUserRoles(reaction.message.author))
+        }
+    })*/
 }
 
 // {
@@ -51,6 +97,7 @@ function commandHandler(message, hasPremissions = false, roles = []) {
 //  callbackFunction: must take one parameter, will be exectued when the user calls the command,
 //  description: a short description on what the command dose.
 //  roles: [] an array of all the roles that can use this command
+//  reactionAddCallback: function called upon once a reaction is added to the message
 // }
 exports.addCommand = function addCommand(params = { commandName, description, callbackFunction, roles: [] }) {
     //Throw errors if not all required parameters are satisfied
@@ -58,5 +105,5 @@ exports.addCommand = function addCommand(params = { commandName, description, ca
     if (params.commandName === undefined) throw new Error('No commandName provided');
     if (params.callbackFunction === undefined) throw new Error('No callbackFunction provided');
 
-    global.commands[params.commandName] = params;
+    global.commands[params.commandName.toLowerCase()] = params;
 }
--- discordBot/index.js
+++ discordBot/index.js
@@ -4,13 +4,13 @@ global.discord = tokens.discord;
 global.botid = tokens.botid;
 
 let bot = require('./botSrc');
-
 global.prefix = '.';
 
 //--// help command //--//
 bot.addCommand({
     commandName: 'help',
     callbackFunction: function(parameters, message, roles) {
+        //Embed for any command with a helpEmbedPage: 0 or none.
         let helpEmbed = {
             color: 0x0099ff,
             title: 'Here are all the available commands!',
@@ -24,15 +24,28 @@ bot.addCommand({
                 value: '\u200b',
                 inline: false,
             }],
-            timestamp: new Date(),
             footer: {
-                text: 'Made by Grzegorz M',
+                text: 'Made by Grzegorz M | [help,0]',
                 icon_url: 'https://cdn.discordapp.com/avatars/892820433592803400/61cdf5225f23d50315ada918b4c4efc8.webp?size=80',
             },
         };
 
-        let count = 0;
+        //Embed for any command with a helpEmbedPage: -1, reserved for admin commands.
+        let adminEmbed = {
+            color: 0x0099ff,
+            title: 'Here are all the available admin commands!',
+            url: 'https://github.com/KetamineKyle/TUDtallaght-Discord-bot',
+            fields: [],
+            footer: {
+                text: 'Made by Grzegorz M | ',
+                icon_url: 'https://cdn.discordapp.com/avatars/892820433592803400/61cdf5225f23d50315ada918b4c4efc8.webp?size=80',
+            },
+        };
+
         //loop tru every command available.
+        let counters = { helpEmbed: 0, adminEmbed: 0 },
+            adminHelp = false;
+
         Object.keys(global.commands).forEach(command => {
             let pass = false,
                 commandObj = global.commands[command];
@@ -45,17 +58,46 @@ bot.addCommand({
             });
 
             if (pass === true) {
-                helpEmbed.fields = [...helpEmbed.fields, {
+                let currentEmbed = helpEmbed;
+                if (commandObj.helpEmbedPage === undefined || commandObj.helpEmbedPage === 0) {
+                    currentEmbed = helpEmbed;
+                    count = ++counters.helpEmbed;
+                } else if (commandObj.helpEmbedPage === -1) {
+                    currentEmbed = adminEmbed;
+                    count = ++counters.adminEmbed;
+                    adminHelp = true;
+                };
+
+                //add command name and description to the embed.
+                currentEmbed.fields = [...currentEmbed.fields, {
                     name: `${global.prefix}${commandObj.commandName}`, //Give the help entry a name
                     value: commandObj.description, //Give the help entry a description
                     inline: function() {
-                        if (++count % 3 === 0) return false; //every seccond item move onto a new line.
+                        if (count % 3 === 0) return false; //every seccond item move onto a new line.
                         else return true;
                     }(),
                 }];
             }
         });
-        message.channel.send({ embeds: [helpEmbed] });
+
+        //Send out the embed
+        message.channel.send({ embeds: [helpEmbed], fetchReply: true }).then(returnedMsg => {
+            //add an close reaction to the embed, only if admin page is disabled.
+            if (adminHelp === false) returnedMsg.react('❌');
+            else if (adminHelp === true) {
+
+                //Add a refrence to the above panel.
+                adminEmbed.footer.text += `[help,0,${returnedMsg.id}]`;
+
+                //Send out the embed with admin commands
+                message.channel.send({ embeds: [adminEmbed], fetchReply: true }).then(returnedMsg => {
+
+                    //add an close reaction to the embed
+                    returnedMsg.react('❌');
+                });
+            }
+
+        });
     },
     description: 'A command that displays all available commands.',
     roles: [
@@ -63,13 +105,61 @@ bot.addCommand({
     ]
 });
 
+
 //--// Manualy authenticate a user //--//
 bot.addCommand({
-    commandName: 'adduser',
+    commandName: 'spawnAuthMsg',
+    callbackFunction: function(parameters, message, roles, allRoles = []) {
+
+        //Return and throw an error if the cahannel Id provied is incorect
+        let channel = global.client.channels.cache.get(parameters[1]);
+        if (channel === undefined) {
+            message.channel.send('Invalid parameter, could not locate the channel.');
+            return
+        }
+
+        //Return and throw an error if the provided role is incorect
+        global.client.guilds.resolve(message.guildId).roles.cache.map(m => allRoles = [...allRoles, m.name]);
+        if (parameters[2] === undefined || allRoles.includes(parameters[2]) === false) {
+            message.channel.send('Invalid parameter, could not locate role.');
+            return;
+        }
+
+        let helpEmbed = {
+            color: 0x0099ff,
+            title: 'Verify your email to gain access to all the features of this bot!',
+            url: 'https://github.com/KetamineKyle/TUDtallaght-Discord-bot',
+            description: 'Welcome to the TUD Tallaght campus discord server! \n Please react with the mail emoji, than I\'ll send you a link in your dm\'s to verify your student email.',
+            thumbnail: {
+                url: 'https://cdn.discordapp.com/avatars/892820433592803400/61cdf5225f23d50315ada918b4c4efc8.webp?size=80',
+            },
+            footer: {
+                text: `Made by Grzegorz M | [spawnAuthMsg,${parameters[2]}]`,
+                icon_url: 'https://cdn.discordapp.com/avatars/892820433592803400/61cdf5225f23d50315ada918b4c4efc8.webp?size=80',
+            },
+        };
+
+        //Send out the embed
+        channel.send({ embeds: [helpEmbed], fetchReply: true }).then(returnedMsg => {
+            //add an reaction to the embed
+            returnedMsg.react('📧');
+        });
+    },
+    helpEmbedPage: -1,
+    description: 'This command spawns the auth msg for users to link their accs.',
+    roles: [
+        'test'
+    ]
+});
+
+//--// Manualy authenticate a user //--//
+bot.addCommand({
+    commandName: 'addUser',
     callbackFunction: function(parameters, message, roles) {
         message.channel.send('poo');
     },
     description: 'This command authenticates a user manualy.',
+    helpEmbedPage: -1,
     roles: [
         'test'
     ]
@@ -77,11 +167,12 @@ bot.addCommand({
 
 //--// removes a users authentication //--//
 bot.addCommand({
-    commandName: 'remuser',
+    commandName: 'remUser',
     callbackFunction: function(parameters, message, roles) {
         message.channel.send('poo');
     },
     description: 'This command removes a users authentication.',
+    helpEmbedPage: -1,
     roles: [
         'test'
     ]
@@ -89,7 +180,7 @@ bot.addCommand({
 
 //--// get timetable //--//
 bot.addCommand({
-    commandName: 'timetable',
+    commandName: 'timeTable',
     callbackFunction: function(parameters, message, roles) {
         message.channel.send('timetable');
     },
@@ -104,9 +195,9 @@ bot.addCommand({
 bot.addCommand({
     commandName: 'next',
     callbackFunction: function(parameters, message, roles) {
-        message.channel.send('timetable');
+        message.channel.send('timeTable');
     },
-    description: 'This command provides you with your timetable.',
+    description: 'This tells you your next class and class number.',
     roles: [
         'user',
         'test'
